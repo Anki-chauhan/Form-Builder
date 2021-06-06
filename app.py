3edc5cd12b98963cb570d1544e02de7e6dbcf2bd
Ankit <cse16308.sbit@gmail.com>
2021-06-06 11:38:19 +0530

    fix the register page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, redirect, url_for, request
 from werkzeug.utils import invalidate_cached_property
 from forms import RegistrationForm,LoginForm
@@ -25,12 +26,20 @@ class Form(db.Model):
 class Registrationform(db.Model):
     id = db.Column("ID",db.Integer, primary_key = True)
     name = db.Column("Name", db.String(20),nullable = False)
+    mobile = db.Column("Mobile", db.Integer,nullable = False)
     email = db.Column("Email", db.String(30), nullable = False)
     password = db.Column("password", db.String(20), nullable  =False)
-    def __init__(self, name, email, password):
+    def __init__(self, name, mobile, email, password):
         self.name = name
         self.email = email
+        self.mobile = mobile
         self.password = password
+
+    def __repr__(self):
+        return f'{self.name},{self.email}'
+
+
+
 
 posts =[
     {
@@ -52,31 +61,73 @@ posts =[
 def home():
     return render_template('home.html',title = 'home' ,posts = posts)
 
+@app.route('/sign_up')
+def sign_up():
+    return render_template('/register.html')
 
-@app.route('/')
+
+@app.route('/', methods = ['GET','POST'])
 def helloworld():
+    if request.method == 'POST':
+        one = request.form['one']
+        two = request.form['two']
+        three = request.form['three']
+        four = request.form['four'] 
+
     return render_template('questions.html') 
 
-@app.route('/log', methods=['GET', 'POST'])
-def log():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     error = None
     if request.method == 'POST':
-        user_name = request.form.get('user_email')
-        user_pass = request.form.get('user_password1')
-        if user_name != 'admin@gmail.com' or user_pass != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('log.html', error=error)
+        user_name = request.form['username']
+        user_pass = request.form['password']
+        names = Registrationform.query.with_entities(Registrationform.name)
+        upass = Registrationform.query.with_entities(Registrationform.password)
+        usr_name = []
+        usr_pass = []
 
-@app.route('/register')
+        for name in names:
+            usr_name.append(name)
+            print(name)
+        for pass1 in upass:
+            usr_pass.append(pass1)
+            print(pass1)
+        for u, p in zip(usr_name, usr_pass):
+            if u == user_name and p == user_pass:
+                error = 'Invalid Credentials. Please try again.'
+            else:
+               return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/register', methods = ['GET','POST'])
 def register():
-    form = RegistrationForm()
+    if request.method == 'POST':
+        name = request.form['name']
+        mob = request.form['mob']
+        email = request.form['email']
+        password = request.form['user_password1']
+        new_user = Registrationform(name = name, mobile=mob ,email = email, password = password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('login')
+        except:
+            return "there is some problem to add new user"
+    else:
+        return render_template("register.html")
+
+
+    
     return render_template('register.html', title = 'registration', form = form)
 
 @app.route('/response')
 def response():
     return render_template('response.html')
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
 
 
 
